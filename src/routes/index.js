d723733c7fbc28aca7a8fb1236d93ae5fa828f07
Andres Felipe Serrano Barrios <andresfserrano1@gmail.com>
2023-01-30 07:12:27 -0500

    Inicio de login

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var ROUTER = express.Router();
+let indexController = require('../controllers/indexController.js');
+
+ROUTER.get('/', indexController.home);
+
+ROUTER.get('/recovery', indexController.recovery);
+
+ROUTER.get('/cart', indexController.cart);
+
+ROUTER.get('/favoritos', indexController.favoritos);
+
+module.exports = ROUTER;
---
+++ src/routes/usersRouter.js
@@ -0,0 +1,34 @@
+const express = require ("express"); 
+const router = express.Router();
+const {body} = require('express-validator');
+validationLogin = [
+body('nombreUsuario').notEmpty().withMessage('Debes completar el campo email').isEmail().withMessage('Debes ingresar un formato de email válido'),
+body('password').notEmpty().withMessage('Debes ingresar la contraseña').isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres')];
+
+const usersController = require('../controllers/usersController.js');
+const uploadFile = require('../middlewares/multerMiddlewareUsers');
+
+//Middlewares
+
+// Consulta de users
+router.get('/', usersController.users);
+
+//Crear Users
+router.get('/create',usersController.create);
+router.post('/new', usersController.newUsers);
+
+// Modificar Users
+router.get('/modificacion/:id',usersController.modificar); // Este sería del admin
+router.put('/edit/:id',uploadFile.single('avatarUserFile') ,usersController.update);
+
+// Delete Users
+router.post('/delete/:id', usersController.delete);
+
+//Login User
+router.get('/login', usersController.login);
+router.post('/login',validationLogin,usersController.checkLogin)
+
+//User logeado
+router.get('/profile',usersController.logged)
+
+module.exports = router;
